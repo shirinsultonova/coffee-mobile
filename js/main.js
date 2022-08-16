4d7f526e022733eb51c143ccb777696421e4b11e
shirin_sultonova <shirin.sultonova@inbox.ru>
2022-08-16 18:06:02 +0500

    add about page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+var elModalOpenButton = document.querySelector(".menu-btn");
+var elModalOpenlink = document.querySelector(".nav__list-link");
+var elModal = document.querySelector(".nav");
+
+elModalOpenButton.addEventListener("click", function () {
+elModal.classList.toggle("nav--active");
+});
